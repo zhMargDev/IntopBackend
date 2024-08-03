4a5f55aad0d3da3e00d70b8ab3a912dbc4778b4a
zhMargDev <zh.margaryan.dev@gmail.com>
2024-08-04 02:22:44 +0400

    Added basedir for user avatars

--- api/config.py
+++ api/config.py
@@ -1,3 +1,4 @@
 SECRET_KEY = '4d45fa4a82082e4175f8c70e7fed1113fe77d325b71c6c338d99b6f9cccbd440'
 TG_BOT_KEY = '7395108105:AAEGCC4hCMHC1k0BkALpcov2Qhyb-2Ht3Ro'
 DB_CONNECT = "postgresql://intop:ai5JeI9ahng1ohV1@localhost:5432/intop_db"
+BASE_DIR = "/static/users_avatars/"
