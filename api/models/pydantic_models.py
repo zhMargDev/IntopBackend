a450916919bb67b23243a4652869b9f41c061b60
zhMargDev <zh.margaryan.dev@gmail.com>
2024-08-03 19:02:20 +0400

    Added category model

--- api/models/pydantic_models.py
+++ api/models/pydantic_models.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import List
 
 # Pydantic модель для получения данных
 class TelegramInitData(BaseModel):
@@ -6,3 +7,12 @@ class TelegramInitData(BaseModel):
     first_name: str
     last_name: str
     username: str
+
+# Схема для ответа по категории
+class CategoryOut(BaseModel):
+    id: int
+    name: str
+    subcategories: List['CategoryOut'] = []
+
+    class Config:
+        orm_mode = True
