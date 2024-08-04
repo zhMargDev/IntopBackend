05c1aaac943761740b030ad796efc24711918806
zhMargDev <zh.margaryan.dev@gmail.com>
2024-08-04 16:44:51 +0400

    The account deactivation router has been added and checked, the account update router has been changed, now the account is deactivated and the data cannot be changed

--- api/routers/users/data.py
+++ api/routers/users/data.py
@@ -1,6 +1,6 @@
 import shutil
 import os
-from fastapi import APIRouter, Depends, HTTPException, Request, File, UploadFile, Form
+from fastapi import APIRouter, Depends, HTTPException, Request, File, UploadFile, Form, Query
 from sqlalchemy.orm import Session
 from starlette.responses import JSONResponse
 
@@ -11,9 +11,45 @@ from config import BASE_DIR
 
 router = APIRouter()
 
-@router.put("/update", summary="Обновление данных пользователя")
+@router.put("/update", summary="Обновление данных пользователя",
+            description="""
+            Обновляет информацию о пользователе. Необходимо отправить форму с данными, которые требуется обновить.
+            При успешном обновлении возвращается сообщение об успешном обновлении.
+
+            **Параметры формы:**
+            - `user_id`: Id пользователя для проверки совместимости с токеном, если не отправить то вернёться ошибка
+            - `first_name`: Имя пользователя (опционально)
+            - `second_name`: Фамилия пользователя (опционально)
+            - `username`: Имя пользователя (опционально)
+            - `phone_number`: Номер телефона (опционально)
+            - `email`: Адрес электронной почты (опционально)
+            - `region_id`: Идентификатор региона (опционально)
+            - `avatar`: Новый аватар пользователя (опционально)
+
+            ```
+            Пример Curl
+            curl -v -X PUT "http://localhost:8000/users/update" \
+              -H "Content-Type: multipart/form-data" \
+              -b "access_token=your_access_token" \
+              -F "user_id"=1 \
+              -F "first_name=John" \
+              -F "second_name=Doe" \
+              -F "username=johndoe" \
+              -F "phone_number=1234567890" \
+              -F "email=john.doe@example.com"
+            ```
+
+            **Ответ:**
+            - Если обновление прошло успешно, возвращается сообщение о успешном обновлении данных.
+
+            **Ошибки:**
+            - `401 Unauthorized`: Токен доступа отсутствует или недействителен.
+            - `401 Unauthorized`: Недействительный пользователь. Отправленный id и id из токена не совпали или id не был отправлен.
+            - `404 Not Found`: Пользователь не найден.
+            """)
 async def update_user(
         request: Request,
+        user_id: int = Form(None),
         first_name: str = Form(None),
         second_name: str = Form(None),
         username: str = Form(None),
@@ -27,19 +63,22 @@ async def update_user(
     token = request.cookies.get("access_token")
 
     if not token:
-        raise HTTPException(status_code=401, detail="Access token is missing")
+        raise HTTPException(status_code=401, detail="Токен доступа отсутствует")
 
     # Получение user_id из токена
     try:
         payload = decode_access_token(token)
-        user_id = int(payload['sub'])
+        token_user_id = int(payload['sub'])
     except HTTPException:
-        raise HTTPException(status_code=401, detail="Invalid token")
+        raise HTTPException(status_code=401, detail="Недействительный токен")
+
+    if not user_id or user_id != token_user_id:
+        raise HTTPException(status_code=401, detail="Недействительный пользователь")
 
     # Найти пользователя по user_id
-    user = db.query(User).filter(User.id == user_id).first()
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
+    user = db.query(User).filter(User.id == token_user_id).first()
+    if not user or not user.is_active:
+        raise HTTPException(status_code=404, detail="Пользователь не найден")
 
     # Обновление данных пользователя
     if first_name:
@@ -58,7 +97,7 @@ async def update_user(
     # Сохранение аватарки, если она предоставлена
     if avatar:
         # Создание пути для нового аватара
-        avatar_filename = f"{user_id}{os.path.splitext(avatar.filename)[1]}"  # Сохранение оригинального расширения
+        avatar_filename = f"{token_user_id}{os.path.splitext(avatar.filename)[1]}"  # Сохранение оригинального расширения
         avatar_path = os.path.join(BASE_DIR, 'static', 'users_avatars', avatar_filename)
 
         # Удаление старого аватара, если он существует
@@ -77,10 +116,68 @@ async def update_user(
     db.commit()
     db.refresh(user)
 
-    print(user.email)
-
     # Обновляем токен и устанавливаем его в куки
-    response = JSONResponse(content={"message": "User data updated successfully"})
+    response = JSONResponse(content={"message": "Данные пользователя успешно обновлены"})
     response = update_token(response, user.id)
+
+    return response
+
+@router.delete('/deactivate', summary="Деактивация (удаление) аккаунта пользователя",
+               description="""
+               Этот эндпоинт деактивирует аккаунт пользователя.
+
+               **Параметры запроса:**
+               - `user_id`: Идентификатор пользователя (необязательный параметр). Если не указан, используется `user_id` из токена доступа.
+
+               **Пример запроса**
+               curl -v -X DELETE "http://localhost:8000/users/deactivate" \
+                  -H "Content-Type: application/json" \
+                  -b "access_token=your_access_token" \
+                  -G \
+                  -d "user_id=1"
+
+               **Ответ:**
+               - Если деактивация успешна, возвращается сообщение о том, что аккаунт успешно деактивирован и токен доступа удаляется из куки.
+               - В случае отсутствия токена, его недействительности или несоответствия `user_id` возвращается ошибка.
+
+               **Ошибки:**
+               - `401 Unauthorized`: Токен доступа отсутствует, недействителен или `user_id` не совпадает.
+               - `404 Not Found`: Пользователь не найден.
+               """)
+async def deactivate_account(
+        request: Request,
+        user_id: int = Query(None),
+        db: Session = Depends(get_db)
+    ):
+    # Извлечение токена из куки
+    token = request.cookies.get("access_token")
+
+    if not token:
+        raise HTTPException(status_code=401, detail="Токен доступа отсутствует")
+
+    try:
+        payload = decode_access_token(token)
+        token_user_id = int(payload['sub'])
+    except HTTPException:
+        raise HTTPException(status_code=401, detail="Недействительный токен")
+
+    # Проверка, если user_id не указан или не совпадает с user_id из токена
+    if not user_id or user_id != token_user_id:
+        raise HTTPException(status_code=401, detail="Пользователь недействителен")
+
+    # Найти пользователя по user_id
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="Пользователь не найден")
+
+    # Деактивировать аккаунт (или удалить его, если требуется)
+    # Например, можно установить флаг активности в базе данных
+    user.is_active = False
+    db.commit()
+    db.refresh(user)
+
+    # Удаление токена из куки
+    response = JSONResponse(content={"message": "Аккаунт успешно деактивирован"})
+    response.set_cookie(key="access_token", value="", expires=0)
 
     return response
