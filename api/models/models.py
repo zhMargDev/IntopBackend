92d619878eecc216f6b0311abeaa6e63bd42b4a9
zhMargDev <zh.margaryan.dev@gmail.com>
2024-08-06 01:05:03 +0400

    Splitting the models.py file: Tabels moved to tables.py, models remained in models.py

--- api/models/models.py
+++ api/models/models.py
@@ -1,104 +1,9 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime, Boolean, MetaData, Table
 from sqlalchemy.orm import declarative_base, relationship
-
-# Создание экземпляра MetaData
-metadata = MetaData()
+from models.tables import (roles_table, users_table, ratings_table, categories_table, 
+                     stores_table, store_emails_table, store_phone_numbers_table, regions_table)
 
 # Создание базового класса с использованием декларативной базы
 Base = declarative_base()
-
-# Определение таблицы 'roles'
-roles_table = Table(
-    'roles',
-    metadata,
-    Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('name', String(255), nullable=False),
-    Column('permission', String(255), nullable=False)
-)
-
-# Определение таблицы 'users'
-users_table = Table(
-    'users',
-    metadata,
-    Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('telegram_id', Integer, unique=True),
-    Column('role_id', Integer, ForeignKey('roles.id')),
-    Column('username', String(255), nullable=True),
-    Column('first_name', String(255), nullable=True),
-    Column('second_name', String(255), nullable=True),
-    Column('phone_number', String(255), nullable=True),
-    Column('email', String(255), nullable=True),
-    Column('avatar', String(255), nullable=True),
-    Column('rating', Float, nullable=True),
-    Column('region_id', Integer, ForeignKey('regions.id'), nullable=True),
-    Column('is_verified', Boolean, default=False),
-    Column('is_active', Boolean, default=True),
-    Column('last_active', DateTime),
-)
-
-# Определение таблицы 'ratings'
-ratings_table = Table(
-    'ratings',
-    metadata,
-    Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('rater_id', Integer, nullable=False),
-    Column('rated_id', Integer, nullable=False),
-    Column('rating', Float, nullable=False)
-)
-
-# Определение таблицы 'categories'
-categories_table = Table(
-    'categories',
-    metadata,
-    Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('name', String(255), nullable=False),
-    Column('parent_id', Integer, ForeignKey('categories.id'), nullable=True)
-)
-
-# Определение таблицы 'stores'
-stores_table = Table(
-    'stores',
-    metadata,
-    Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('name', String(255), nullable=False),
-    Column('short_name', String(100), nullable=False),
-    Column('llc_name', String(255), nullable=True),
-    Column('address', String(255), nullable=True),
-    Column('region_id', Integer, ForeignKey('regions.id'), nullable=True),
-    Column('category_id', Integer, ForeignKey('categories.id'), nullable=False),
-    Column('owner_id', Integer, ForeignKey('users.id')),
-    Column('rating', Float, nullable=True),
-    Column('is_verified', Boolean, default=False)
-)
-
-# Определение таблицы 'store_emails'
-store_emails_table = Table(
-    'store_emails',
-    metadata,
-    Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('store_id', Integer, ForeignKey('stores.id')),
-    Column('email', String(255), nullable=False),
-    Column('email_type', String(50), nullable=False)
-)
-
-# Определение таблицы 'store_phone_numbers'
-store_phone_numbers_table = Table(
-    'store_phone_numbers',
-    metadata,
-    Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('store_id', Integer, ForeignKey('stores.id')),
-    Column('phone_number', String(20), nullable=False),
-    Column('phone_type', String(50), nullable=False)
-)
-
-# Определение таблицы 'regions'
-regions_table = Table(
-    'regions',
-    metadata,
-    Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('name', String(255), nullable=False),
-    Column('parent_id', Integer, ForeignKey('regions.id'), nullable=True)
-)
 
 # Определение класса Role, связанного с таблицей 'roles'
 class Role(Base):
@@ -170,8 +75,8 @@ class Store(Base):
     is_verified = stores_table.c.is_verified
 
     # Связь с другими таблицами
-    owner = relationship(User, backref='stores')
-    category = relationship(Category, backref='stores')
+    owner = relationship('User', backref='stores')
+    category = relationship('Category', backref='stores')
     region = relationship('Region', backref='stores')
 
 # Определение класса StoreEmail, связанного с таблицей 'store_emails'
@@ -184,7 +89,7 @@ class StoreEmail(Base):
     email_type = store_emails_table.c.email_type
 
     # Связь с Store
-    store = relationship(Store, backref='emails')
+    store = relationship('Store', backref='emails')
 
 # Определение класса StorePhoneNumber, связанного с таблицей 'store_phone_numbers'
 class StorePhoneNumber(Base):
@@ -196,7 +101,7 @@ class StorePhoneNumber(Base):
     phone_type = store_phone_numbers_table.c.phone_type
 
     # Связь с Store
-    store = relationship(Store, backref='phone_numbers')
+    store = relationship('Store', backref='phone_numbers')
 
 # Определение класса Region, связанного с таблицей 'regions'
 class Region(Base):
---
+++ api/models/tables.py
@@ -0,0 +1,97 @@
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime, Boolean, MetaData, Table
+
+# Создание экземпляра MetaData
+metadata = MetaData()
+
+# Определение таблицы 'roles'
+roles_table = Table(
+    'roles',
+    metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('name', String(255), nullable=False),
+    Column('permission', String(255), nullable=False)
+)
+
+# Определение таблицы 'users'
+users_table = Table(
+    'users',
+    metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('telegram_id', Integer, unique=True),
+    Column('role_id', Integer, ForeignKey('roles.id')),
+    Column('username', String(255), nullable=True),
+    Column('first_name', String(255), nullable=True),
+    Column('second_name', String(255), nullable=True),
+    Column('phone_number', String(255), nullable=True),
+    Column('email', String(255), nullable=True),
+    Column('avatar', String(255), nullable=True),
+    Column('rating', Float, nullable=True),
+    Column('region_id', Integer, ForeignKey('regions.id'), nullable=True),
+    Column('is_verified', Boolean, default=False),
+    Column('is_active', Boolean, default=True),
+    Column('last_active', DateTime),
+)
+
+# Определение таблицы 'ratings'
+ratings_table = Table(
+    'ratings',
+    metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('rater_id', Integer, nullable=False),
+    Column('rated_id', Integer, nullable=False),
+    Column('rating', Float, nullable=False)
+)
+
+# Определение таблицы 'categories'
+categories_table = Table(
+    'categories',
+    metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('name', String(255), nullable=False),
+    Column('parent_id', Integer, ForeignKey('categories.id'), nullable=True)
+)
+
+# Определение таблицы 'stores'
+stores_table = Table(
+    'stores',
+    metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('name', String(255), nullable=False),
+    Column('short_name', String(100), nullable=False),
+    Column('llc_name', String(255), nullable=True),
+    Column('address', String(255), nullable=True),
+    Column('region_id', Integer, ForeignKey('regions.id'), nullable=True),
+    Column('category_id', Integer, ForeignKey('categories.id'), nullable=False),
+    Column('owner_id', Integer, ForeignKey('users.id')),
+    Column('rating', Float, nullable=True),
+    Column('is_verified', Boolean, default=False)
+)
+
+# Определение таблицы 'store_emails'
+store_emails_table = Table(
+    'store_emails',
+    metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('store_id', Integer, ForeignKey('stores.id')),
+    Column('email', String(255), nullable=False),
+    Column('email_type', String(50), nullable=False)
+)
+
+# Определение таблицы 'store_phone_numbers'
+store_phone_numbers_table = Table(
+    'store_phone_numbers',
+    metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('store_id', Integer, ForeignKey('stores.id')),
+    Column('phone_number', String(20), nullable=False),
+    Column('phone_type', String(50), nullable=False)
+)
+
+# Определение таблицы 'regions'
+regions_table = Table(
+    'regions',
+    metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('name', String(255), nullable=False),
+    Column('parent_id', Integer, ForeignKey('regions.id'), nullable=True)
+)
